7f1243592327b427e57645ab435ff9471b11a30a
DuyTung13 <66304228+DuyTung13@users.noreply.github.com>
2021-04-19 15:31:05 +0700

    Delete src directory

--- src/com/vti/Answer.java
+++
@@ -1,8 +0,0 @@
-package com.vti;
-
-public class Answer {
-	int ID;
-	String content;
-	Question Question;
-	boolean isCorrect;
-}
--- src/com/vti/TypeQuestion.java
+++
@@ -1,6 +0,0 @@
-package com.vti;
-
-public class TypeQuestion {
-	int ID;
-	TypeName Name;
-}
