80383cf9e91f41c4a25c44b2800849833587a34d
DuyTung13 <66304228+DuyTung13@users.noreply.github.com>
2021-04-22 22:06:24 +0700

    Testing_System_4

---
+++ src/com/vti/entity/GroupAccount.java
@@ -0,0 +1,9 @@
+package com.vti.entity;
+
+import java.time.LocalDate;
+
+public class GroupAccount {
+	public Group[] Group;
+	public Account[] Account;
+	public LocalDate JoinDate;
+}
---
+++ src/com/vti/frontent/Demo1.java
@@ -0,0 +1,15 @@
+package com.vti.frontent;
+
+import com.vti.backent.Exercise_1;
+
+public class Demo1 {
+	public static void main(String[] args) {
+		Exercise_1 ex1 = new Exercise_1();
+		ex1.question_1();
+		
+		ex1.question_2();
+		
+		ex1.question3();
+	}
+
+}
