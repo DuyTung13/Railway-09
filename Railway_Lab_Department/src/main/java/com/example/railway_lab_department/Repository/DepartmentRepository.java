b3d74c87b4743b7f0a31e109f240185fae6a4884
DuyTung13 <66304228+DuyTung13@users.noreply.github.com>
2021-06-14 19:22:32 +0700

    Delete Railway_Lab_Department directory

--- Railway_Lab_Department/src/main/java/com/example/railway_lab_department/Repository/DepartmentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.railway_lab_department.Repository;
-
-import com.example.railway_lab_department.entity.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DepartmentRepository extends JpaRepository<Department, Long> {
-}
